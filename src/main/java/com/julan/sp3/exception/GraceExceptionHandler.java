bf8e039d1ecefbfa0db708b0733c0129c40d5cba
Hui Lee <lihui870920@gmail.com>
2023-08-23 11:00:23 +0800

    声明数据验证规则

--- src/main/java/com/julan/sp3/exception/GraceExceptionHandler.java
+++ src/main/java/com/julan/sp3/exception/GraceExceptionHandler.java
@@ -2,16 +2,12 @@ package com.julan.sp3.exception;
 
 import com.julan.sp3.util.api.ResultJson;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GraceExceptionHandler {
@@ -26,26 +22,22 @@ public class GraceExceptionHandler {
         return ResultJson.failed("数据不存在");
     }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResultJson<Object> handler(MethodArgumentNotValidException e) {
-//        BindingResult bindingResult = e.getBindingResult();
-//        HashMap<String, String> map = new HashMap<>();
-//        List<FieldError> errors = bindingResult.getFieldErrors();
-//        for (FieldError error : errors) {
-//            String field = error.getField();
-//            String message = error.getDefaultMessage();
-//            map.put(field, message);
-//        }
-//        return ResultJson.validateFailed(map);
-//    }
-
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResultJson<Object> handler(MethodArgumentNotValidException e) {
+        //全量显示，显示不优雅
+        //        HashMap<String, String> map = new HashMap<>();
+        //        List<FieldError> errors = e.getBindingResult().getFieldErrors();
+        //        for (FieldError error : errors) {
+        //            String field = error.getField();
+        //            String message = error.getDefaultMessage();
+        //            map.put(field, message);
+        //        }
+        //        return ResultJson.validateFailed(map);
+        //单条显示，优雅
         List<FieldError> errors = e.getFieldErrors();
         return ResultJson.validateFailed(errors.get(0).getDefaultMessage());
     }
-
 
     @ExceptionHandler(Exception.class)
     public ResultJson<Object> handler(Exception e) {
