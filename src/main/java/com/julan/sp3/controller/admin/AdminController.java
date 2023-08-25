f90ca0669b3eaa60770a1bc0b561c15d0a6ac624
Hui Lee <lihui870920@gmail.com>
2023-08-25 18:48:05 +0800

    修改项目结构

--- src/main/java/com/julan/sp3/controller/admin/AdminController.java
+++ src/main/java/com/julan/sp3/controller/admin/AdminController.java
@@ -4,11 +4,13 @@ import com.julan.sp3.model.entity.User;
 import com.julan.sp3.repository.user.UserRepository;
 import com.julan.sp3.util.api.ResultJson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -17,6 +19,17 @@ public class AdminController {
 
     @Autowired
     private UserRepository userRepository;
+
+
+    @Value("${custom.name}")
+    private String name;
+
+    @Value("${custom.author}")
+    private List<String> author;
+
+
+//    @Value("${custom.users}")
+//    private List<Object> users;
 
 
     @GetMapping
@@ -28,7 +41,13 @@ public class AdminController {
     @GetMapping("/api")
     @ResponseBody
     public ResultJson<Object> api() {
-        return ResultJson.success();
+        HashMap<String, Object> hashMap = new HashMap<>();
+
+        hashMap.put("name", name);
+//        hashMap.put("author", author);
+//        hashMap.put("users", users);
+
+        return ResultJson.success(hashMap);
     }
 
 }
