3ec8dac68d0890d0110f2839ab0d9e8eb0ec6429
Hui Lee <lihui870920@gmail.com>
2023-08-26 08:47:44 +0800

    优雅一点的配置YAML

--- src/main/java/com/julan/sp3/config/CustomConfig.java
+++ src/main/java/com/julan/sp3/config/CustomConfig.java
@@ -2,13 +2,13 @@ package com.julan.sp3.config;
 
 import com.julan.sp3.util.factory.YmlConfigFactory;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
 
 @Data
@@ -22,5 +22,13 @@ public class CustomConfig {
 
     public List<String> author;
 
-    public List<Map<String, String>> users;
+    public List<user> users;
+
+    @Data
+    public static class user {
+        private int sex;
+        private String name;
+        private String password;
+    }
+    
 }
--- src/main/java/com/julan/sp3/controller/admin/AdminController.java
+++ src/main/java/com/julan/sp3/controller/admin/AdminController.java
@@ -4,13 +4,13 @@ import com.julan.sp3.config.CustomConfig;
 import com.julan.sp3.model.entity.User;
 import com.julan.sp3.repository.user.UserRepository;
 import com.julan.sp3.util.api.ResultJson;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -33,13 +33,13 @@ public class AdminController {
     @GetMapping("/api")
     @ResponseBody
     public ResultJson<Object> api() {
-        HashMap<String, Object> hashMap = new HashMap<>();
+//        HashMap<String, Object> hashMap = new HashMap<>();
+//
+//        hashMap.put("name", customConfig.getName());
+//        hashMap.put("author", customConfig.getAuthor());
+//        hashMap.put("users", customConfig.getUsers());
 
-        hashMap.put("name", customConfig.getName());
-        hashMap.put("author", customConfig.getAuthor());
-        hashMap.put("users", customConfig.getUsers());
-
-        return ResultJson.success(hashMap);
+        return ResultJson.success(customConfig.getUsers());
     }
 
 }
