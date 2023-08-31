be950e5f3590e278adf65bae664ed2d2f127849b
李辉 <lihui870920@gmail.com>
2023-08-31 12:39:45 +0800

    数据库DB方式操作

--- src/main/java/com/julan/sp3/controller/api/APIController.java
+++ src/main/java/com/julan/sp3/controller/api/APIController.java
@@ -8,10 +8,12 @@ import jakarta.annotation.Resource;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,6 +22,9 @@ public class APIController {
 
     @PersistenceContext
     EntityManager entityManager;
+
+    @Resource
+    JdbcTemplate jdbcTemplate;
 
     @GetMapping
     @ResponseBody
@@ -32,10 +37,13 @@ public class APIController {
     @ResponseBody
     @DataPermission(value = "user.list", entity = "User")
     public ResultJson<Object> store() {
-
-
-        Query query = entityManager.createNativeQuery("SELECT * FROM User WHERE id!=:id", User.class);
-        query.setParameter("id", 65L);
-        return ResultJson.success(query.getResultList());
+//        Query query = entityManager.createNativeQuery("SELECT * FROM User WHERE id!=:id and mobile = :mobile", User.class);
+//        query.setParameter("id", 65L).setParameter("mobile", "13554864077");
+//        return ResultJson.success(query.getResultList());
+        //查单条
+//        Map<String, Object> user = jdbcTemplate.queryForMap("Select * from user where id=? and mobile=? limit 1", 39, "13554864077");
+        //查多条
+        List<Map<String, Object>> users = jdbcTemplate.queryForList("Select * from user where keywords like ?", "%HuiLee%");
+        return ResultJson.success(users);
     }
 }
